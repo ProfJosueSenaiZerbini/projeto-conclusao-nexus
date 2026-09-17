58cee1f3809cb593acae816382d95766aaeda95f
FeNogueira66 <fecostanogueira@gmail.com>
2026-09-17 14:34:14 -0300

    Conexão da página de cadastro ao banco de dados e adicionamos a função para se cadastrar

--- routes/cadastroRoutes.js
+++ routes/cadastroRoutes.js
@@ -1,37 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
-const { executarQuery } = require('../db/dbConnect')
+const { executarQuery } = require('../db/dbConnect');
 
-/* Rota para exibir a tela de cadastro */
+/* Rota GET: exibe a tela de cadastro */
 router.get("/", (req, res) => {
-    res.render('cadastro', {mensagemErro: null});
+    res.render('cadastro', { mensagemErro: null });
 });
 
-/* POST para processar o formulário enviado */
+
+/* Rota POST: recebe os dados do formulário */
 router.post("/", async (req, res) => {
-    const { nome, email, cep, cpf, senha } = req.body;
+
+    const { nome, email, cpf, senha } = req.body;
 
     try {
-        //Verifica se o e-mail já está cadastrado
+
+        // Verifica se o e-mail já está cadastrado
         const usuarioExistente = await executarQuery(
             "SELECT * FROM USUARIO WHERE email = ?",
             [email]
         );
 
-        if (usuarioExistente.length > 0){
+        if (usuarioExistente.length > 0) {
+
             return res.render('cadastro', {
                 mensagemErro: "Este e-mail já está cadastrado."
             });
+
         }
 
-        // Cadastra o novo usuário
-        await executarQuery(
-            "INSERT INTO USUARIO (nome, email, senha) VALUES (?, ?, ?)",
-            [nome,email,senha]
+
+        // Verifica se o CPF já está cadastrado
+        const cpfExistente = await executarQuery(
+            "SELECT * FROM USUARIO WHERE cpf = ?",
+            [cpf]
         );
 
-        //Depois do cadastro, volta para o login
+        if (cpfExistente.length > 0) {
+
+            return res.render('cadastro', {
+                mensagemErro: "Este CPF já está cadastrado."
+            });
+
+        }
+
+        // Insere o novo usuário no banco
+        await executarQuery(
+            `INSERT INTO USUARIO
+            (nome, email, cpf, senha)
+            VALUES (?, ?, ?, ?)`,
+            [nome, email, cpf, senha]
+        );
+
+        // Após cadastrar, volta para o login
         res.redirect('/login');
 
     } catch (erro) {
@@ -41,7 +63,9 @@ router.post("/", async (req, res) => {
         res.render('cadastro', {
             mensagemErro: "Erro ao realizar o cadastro."
         });
+
     }
+
 });
 
 module.exports = router;
